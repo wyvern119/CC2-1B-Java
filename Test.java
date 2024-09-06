fcf269e41ab4e777d78e5592b0c9d5eda97ea7bb
wyvern119 <60021836+wyvern119@users.noreply.github.com>
2024-09-05 12:45:58 -1200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc21b.padilla;
+
+/**
+ *
+ * @author m303user
+ */
+
+/*Renzo Padilla 
+September 6, 2024 
+First Programming Session */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Madam Anna \t\tRhodora \nQuitaleg");
+        
+        // Java Escape Characters
+        
+        System.out.println("\n\n\nAnna\"s Class");
+        System.out.println("I Love Java Programming");
+        
+        /* Java Commands: 
+        \t = tab, \n = new line, \" = to display double quote in println
+        // = single-line escape character, /* = multiple-line 
+        */
+    }
+}
