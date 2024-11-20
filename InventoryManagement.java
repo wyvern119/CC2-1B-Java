a7d0f4e3d7ba77544ed5175fcf04746a5edfc002
wyvern119 <60021836+wyvern119@users.noreply.github.com>
2024-11-20 08:02:05 +0800

    added documentation

--- InventoryManagement.java
+++ InventoryManagement.java
@@ -1,11 +1,23 @@
+/*
+Padilla, Renzo Ymanuel L.
+Final Challenge 1: Inventory Management
+November 8, 2024
+*/
+
+//imports scanner
 import java.util.Scanner;
 
 public class InventoryManagement {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int[] price = new int[10];
-        int[]quantity = new int[10];
         
+        //declaration of scanner for storing input
+        Scanner input = new Scanner(System.in);
+        
+        //initialization of arrays
+        int[] price = new int[10];
+        int[] quantity = new int[10];
+        
+        //loop for storing the stock quantity and cost
         for (int ctr = 0; ctr < 10; ctr++) {
             System.out.print("Enter Stock Level [" + (ctr+1) + "]: ");
             price[ctr] = input.nextInt();
@@ -13,6 +25,8 @@ public class InventoryManagement {
             System.out.print("Enter Stock Quantity [" + (ctr+1) + "]: ");
             quantity[ctr] = input.nextInt();
         }
+        
+        //loops through the arrays to calculate the total value and displays output
         for (int ctr = 0; ctr < 10; ctr++) {
             int tValue = price[ctr] * quantity[ctr];
             System.out.println("Total Value[" + (ctr+1) + "]: " + tValue);
