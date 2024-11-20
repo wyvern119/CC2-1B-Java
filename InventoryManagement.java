eeb1b077b96c0ce9e2fab233bffa02fce5d13f10
wyvern119 <60021836+wyvern119@users.noreply.github.com>
2024-11-20 08:35:18 +0800

    added predefined version within code

--- InventoryManagement.java
+++ InventoryManagement.java
@@ -10,6 +10,20 @@ import java.util.Scanner;
 public class InventoryManagement {
     public static void main(String[] args){
         
+        //version 1: predefined
+        /*
+        //initialization of arrays
+        int[] price = {200, 100, 350, 320, 190, 140, 40, 50, 65, 120};
+        int[] quantity = {10, 6, 8, 3, 7, 9, 3, 7, 6, 1};
+        
+        //loops through the arrays to calculate the total value and displays output
+        for (int ctr = 0; ctr < 10; ctr++) {
+            int tValue = price[ctr] * quantity[ctr];
+            System.out.println("Total Value[" + (ctr+1) + "]: " + tValue);
+        }
+        */
+        
+        //version 2: with user input
         //declaration of scanner for storing input
         Scanner input = new Scanner(System.in);
         
