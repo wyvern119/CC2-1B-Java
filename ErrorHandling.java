cac8d27b90cd2ce6a56d9f4ac3dc749b0ad40a40
wyvern119 <60021836+wyvern119@users.noreply.github.com>
2024-12-03 13:00:55 +0800

    final

--- ErrorHandling.java
+++ ErrorHandling.java
@@ -14,9 +14,16 @@ import java.util.Scanner;
 //imports package for checking invalid input error
 import java.util.InputMismatchException;
 
-//creation of custom exception
+//creation of custom exception for exceeded amount
 class InsufficientFundsException extends Exception {
     public InsufficientFundsException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+//creation of custom exception for checking quantity
+class InvalidQuantityException extends Exception {
+    public InvalidQuantityException(String errorMessage) {
         super(errorMessage);
     }
 }
@@ -29,10 +36,21 @@ public class ErrorHandling {
             throw new InsufficientFundsException("\nError! Amount Exceeded!");
         } else {
             System.out.println("\nFunds Successfully Withdrawn!");
+        }
     }
-}
+
+    //exception checks whether the amount is less than 0 or greater than the available amount
+    static void InvalidQuantity(int amount, int quantity) throws InvalidQuantityException {
+        if (amount <= 0 || amount > quantity) {
+            throw new InvalidQuantityException("\nError! Invalid Quantity");
+        } else {
+            System.out.println("Item Added to Cart!");
+        }
+    }
 
     public static void main(String[] args){
+
+        /*
         
         //initialization of scanner
         Scanner input = new Scanner(System.in);
@@ -80,5 +98,41 @@ public class ErrorHandling {
             System.out.println("Account Total Amount: " + currStr);
             System.out.println("Thank you for using our services!");
         }
+        */
+
+        //initialization of scanner
+        Scanner input = new Scanner(System.in);
+
+        //initialization of pre-defined amount of account
+        int quantity = 5;
+        int amount =0;
+
+        try {
+
+            //asks the user to enter amount to put in cart
+            System.out.println("Steam Deck OLED 1 TB");
+            System.out.println("Price: ₱41,595");
+            System.out.println("Quantity: " + quantity);
+            System.out.print("\nAdd to Cart: ");
+            amount = input.nextInt();
+
+            //checks whether the input is greater or lower than the input
+            InvalidQuantity(amount, quantity);
+
+            //catches various errors
+        } catch (InvalidQuantityException e) {
+            //catches error when amount entered exceeds or is lesser than what is available
+            System.out.println("Error! Amount Invalid");
+            amount = 0;
+        } catch (InputMismatchException e) {
+            //catches error when input is anything but numbers
+            System.out.println("Error! Invalid Input");
+        } finally {
+
+            //displays the total amount of cart
+            System.out.println("\nCart Amount: " + amount + " Item/s");
+            System.out.println("Thank you for shopping!");
+        }
     }
 }
+
